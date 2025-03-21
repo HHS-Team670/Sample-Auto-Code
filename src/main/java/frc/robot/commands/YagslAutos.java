b7656d3e01190631e34e400d4610319dfaf9e287
Yim Hoseon <justin.yim14@gmail.com>
2025-03-20 20:24:19 -0700

    feat: check if libraries work

--- src/main/java/frc/robot/commands/YagslAutos.java
+++ src/main/java/frc/robot/commands/YagslAutos.java
@@ -1,5 +1,30 @@
 package frc.robot.commands;
 
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import swervelib.SwerveController;
+import swervelib.SwerveDrive;
+import swervelib.SwerveDriveTest;
+import swervelib.math.SwerveMath;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
 public class YagslAutos {
-    
+    public static class AutoDrive extends Command{
+        @Override
+        public void initialize() {
+        
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+
+    }
 }
